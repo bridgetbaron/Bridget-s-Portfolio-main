af40b38afe08303728d77f16732af64a733a22f2
bridgetbaron <bridgetelizabeth04@gmail.com>
2026-03-13 12:33:47 -0400

    up

---
+++ astro.config.mjs
@@ -0,0 +1,7 @@
+// @ts-check
+import { defineConfig } from "astro/config";
+
+// https://astro.build/config
+export default defineConfig({
+  site: "https://bridgetbaron.github.io/Bridget-s-Portfolio-main",
+});
